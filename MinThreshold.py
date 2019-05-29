1609629086dce50a91a3def61f0676a7d27c10fb
Inusoy <machnikowski@gmail.com>
2019-05-29 22:44:20 +0200

    Created MinThreshold Added r - minimum threshold value

--- MinThreshold.py
+++ MinThreshold.py
@@ -2,7 +2,7 @@ import Processor as pr
 import numpy as np
 
 
-def maxThreshold(process_list, N, p, time_max):
+def minThreshold(process_list, N, p, r, time_max):
     # N - number of processors (50-100)
     # p - threshold of processor utilization (process will be send there if utilization in smaller than p)
     # z - maximum amount of queries for processor utilization
@@ -37,13 +37,18 @@ def maxThreshold(process_list, N, p, time_max):
                     processor_list[process.starting_proc].processes_running.append(process)
 
         for processor in processor_list:  # update running time of all processes on all processors
+            if processor.utilization < r:
+                random_processor = np.random.randint(0, N)
+                if processor.utilization < processor_list[random_processor].calc_util():
+                    processor.processes_running.append(processor_list[random_processor].processes_running[0])
+                    del processor_list[random_processor].processes_running[0]
             for process_running in processor.processes_running:
                 process_running.time_required -= 1
                 if process_running.time_required == 0:
                     processor.processes_running.remove(process_running)
             processor.update_mean_util()  # update mean processor load of each processor
 
-    print("Maximum Threshold processor request, average utilization of each processor:")
+    print("Minimum Threshold processor request, average utilization of each processor:")
     for temp in range(len(processor_list)):
         # print("Processor {0}: {1:3.2f}%".format(temp, processor_list[temp].get_mean_util()))
         sum_of_means += processor_list[temp].get_mean_util()
--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import ProcessGenerator as pg
 import Random as rd
-import MaxThreshold as MT
+import MaxThreshold as MxTh
+import MinThreshold as MiTh
 import copy
 
+"""
+Processor scheduling in distributed systems (processor assignment for a process) simulation of algorithms: 
+random
+maximum threshold
+minimum threshold
+Compare CPU load, number of requests sent, number of process migrations. 
+"""
+
+
 N = 100  # number of processors
-p = 90  # threshold
+p = 90  # maximum threshold
+r = 40  # minimum threshold
 z = 3  # number of retries
 processes = 2000  # amount of processes
 process_list = []
-process_list2 = []
 
 for i in range(processes):
     temp_process = pg.ProcessGen(N)
     process_list.append(temp_process)
 process_list2 = copy.deepcopy(process_list)
+process_list3 = copy.deepcopy(process_list)
 
 TIME_MAX = 1500
 
-MT.maxThreshold(process_list, N, p, TIME_MAX)
+rd.random(process_list, N, p, z, TIME_MAX)
 print()
-rd.random(process_list2, N, p, z, TIME_MAX)
+MxTh.maxThreshold(process_list2, N, p, TIME_MAX)
 print()
+MiTh.minThreshold(process_list3, N, p, r, TIME_MAX)
