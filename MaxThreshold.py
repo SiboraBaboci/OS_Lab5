49d838f0fd5fadc1b7c5c56c5418f73ebd754d3e
Inusoy <machnikowski@gmail.com>
2019-05-29 22:32:27 +0200

    Created MaxThreshold Added deep copy to emulate pass-by-value

--- MaxThreshold.py
+++ MaxThreshold.py
@@ -2,7 +2,7 @@ import Processor as pr
 import numpy as np
 
 
-def random(process_list, N, p, z, time_max):
+def maxThreshold(process_list, N, p, time_max):
     # N - number of processors (50-100)
     # p - threshold of processor utilization (process will be send there if utilization in smaller than p)
     # z - maximum amount of queries for processor utilization
@@ -19,19 +19,23 @@ def random(process_list, N, p, z, time_max):
         for process in process_list:
             process.starting_time -= 1  # decrease starting time of all processes in queue
             if process.starting_time == 0:  # if a process starting time reaches 0, it's added to a random processor
-                z_temp = z  # keep track of how many times you've asked for other processes utilization
-                while z_temp:
-                    random_processor = np.random.randint(0, N)  # randomly choose a processor
-                    sum_of_requests+=1
-                    if processor_list[random_processor].calc_util() < p:  # ask for its utilization, and if it's lower than p
-                        processor_list[random_processor].processes_running.append(process)  # give it that process
-                        sum_of_process_migrations += 1
-                        break
-                    else:
-                        z_temp -= 1
-                        if z_temp == 0:  # if you've asked 'p' processors, process it yourself
-                            processor_list[process.starting_proc].processes_running.append(process)
-
+                p_temp = p  # keep track of how many times you've asked for other processes utilization
+                times_repeated = 0
+                if processor_list[process.starting_proc].utilization > p:
+                    while p_temp:
+                        times_repeated += 1
+                        random_processor = np.random.randint(0, N)  # randomly choose a processor
+                        sum_of_requests += 1
+                        if processor_list[random_processor].calc_util() < p_temp:  # ask for its utilization, and if it's lower than p
+                            processor_list[random_processor].processes_running.append(process)  # give it that process
+                            sum_of_process_migrations += 1
+                            break
+                        else:
+                            if times_repeated == N:
+                                p_temp += 1
+                                times_repeated = 0
+                else:
+                    processor_list[process.starting_proc].processes_running.append(process)
         for processor in processor_list:  # update running time of all processes on all processors
             for process_running in processor.processes_running:
                 process_running.time_required -= 1
@@ -39,11 +43,11 @@ def random(process_list, N, p, z, time_max):
                     processor.processes_running.remove(process_running)
             processor.update_mean_util()  # update mean processor load of each processor
 
-    print("Random processor request, average utilization of each processor:")
+    print("Maximum Threshold processor request, average utilization of each processor:")
     for temp in range(len(processor_list)):
         # print("Processor {0}: {1:3.2f}%".format(temp, processor_list[temp].get_mean_util()))
         sum_of_means += processor_list[temp].get_mean_util()
-    print("Mean utilization of all processes: {0:3.2f}%".format(sum_of_means/N))
+    print("Mean utilization of all processes: {0:3.2f}%".format(sum_of_means / N))
     print("Number of requests sent: {0}".format(sum_of_requests))
     print("Number of processes migrated: {0}".format(sum_of_process_migrations))
     return
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import ProcessGenerator as pg
 import Random as rd
+import MaxThreshold as MT
+import copy
 
 N = 100  # number of processors
-p = 80  # threshold
+p = 90  # threshold
 z = 3  # number of retries
 processes = 2000  # amount of processes
 process_list = []
+process_list2 = []
+
 for i in range(processes):
-    process_list.append(pg.ProcessGen(N))
+    temp_process = pg.ProcessGen(N)
+    process_list.append(temp_process)
+process_list2 = copy.deepcopy(process_list)
 
 TIME_MAX = 1500
 
-print("Random processor request, average utilization of each processor:")
-rd.random(process_list, N, p, z, TIME_MAX)
+MT.maxThreshold(process_list, N, p, TIME_MAX)
+print()
+rd.random(process_list2, N, p, z, TIME_MAX)
+print()
